f307e5d01bb6470dd019dffd4ea3460c828ed906
sudharshinitatrakallu-14 <sudharshinitatrakallu@gmail.com>
2026-07-05 19:06:32 +0530

    Update ocr.py

--- utils/ocr.py
+++ utils/ocr.py
@@ -2,9 +2,13 @@ import easyocr
 import numpy as np
 from PIL import Image
 
-reader = easyocr.Reader(['en'])
+reader = None
 
 def extract_text(uploaded_file):
+    global reader
+
+    if reader is None:
+        reader = easyocr.Reader(['en'])
 
     image = Image.open(uploaded_file)
     image = np.array(image)
@@ -12,7 +16,6 @@ def extract_text(uploaded_file):
     results = reader.readtext(image)
 
     text = ""
-
     for result in results:
         text += result[1] + "\n"
 
